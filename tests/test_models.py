675fba15dacffddf45211984e2277fdca832596b
s-weigand <s.weigand.phy@gmail.com>
2019-04-09 19:15:15 +0200

    added djangocms-helper to dev requirements and used its coockiecutter to create test files

---
+++ tests/test_models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from djangocms_equation import models
+
+from .base import BaseDjangocms_equation
+
+
+class TestDjangocms_equation(BaseDjangocms_equation):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
