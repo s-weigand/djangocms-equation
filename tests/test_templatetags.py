3c1e809b0c7ffa7ba856c66c2763e1b9625b89dc
Sebastian Weigand <s.weigand.phy@gmail.com>
2019-04-28 17:49:00 +0200

    fixed flake8 error

--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
-from djangocms_equation.templatetags.djangocms_equation_tags import format_float_dot_delimiter
+from djangocms_equation.templatetags.djangocms_equation_tags import (
+    format_float_dot_delimiter,
+)
 from django.test import TestCase
+
 
 class TestTemplateTags(TestCase):
     def test_format_float_dot_delimiter(self):
