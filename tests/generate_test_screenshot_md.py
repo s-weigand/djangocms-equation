59a1e1a93593da4d6e885fa416fed9ccbec4c879
s-weigand <s.weigand.phy@gmail.com>
2019-05-21 22:38:36 +0200

    refined report for screenshots to accually work

--- tests/generate_test_screenshot_md.py
+++ tests/generate_test_screenshot_md.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 import os
+from urllib.parse import quote
 
 
 def get_screenshot_test_base_folder():
@@ -13,7 +14,7 @@ def get_screenshot_test_base_folder():
     return dir_path
 
 
-def generate_test_screenshot_md():
+def generate_test_screenshot_report():
     test_env_name = os.getenv("TOX_ENV_NAME", "stand alone")
     screen_shot_dict = {}
     base_path = get_screenshot_test_base_folder()
@@ -22,11 +23,17 @@ def generate_test_screenshot_md():
             if filename.endswith(".png"):
                 dirname = os.path.split(root)[-1]
                 screenshot_caption = os.path.splitext(filename)[0]
-                if not base_path.endswith(dirname):
-                    screenshot_path = "./{}/{}".format(dirname, filename)
+                if "TRAVIS" in os.environ:
+                    if not base_path.endswith(dirname):
+                        screenshot_path = "./{}/{}".format(dirname, quote(filename))
 
+                    else:
+                        screenshot_path = "./{}".format(quote(filename))
                 else:
-                    screenshot_path = "./{}".format(filename)
+                    screenshot_path = os.path.abspath(os.path.join(root, quote(filename)))
+                    screenshot_path = r"file:///{}".format(screenshot_path)
+
+                # screenshot_path = quote(screenshot_path)
                 if dirname not in screen_shot_dict:
                     screen_shot_dict[dirname] = [
                         {
@@ -44,22 +51,22 @@ def generate_test_screenshot_md():
                         }
                     ]
 
-    md_path = os.path.join(base_path, "test_screenshots.md")
-    md_img_template = """
-| ![{filename}]({path} "{caption}") |
-|:--:|
-| *{caption}* |
-
-"""
-    with open(md_path, "w+") as screenshot_md:
-        screenshot_md.write("# Screenshots of test-env: {}\n".format(test_env_name))
+    report_path = os.path.join(base_path, "test_screenshots.html")
+    html_img_template = r"""
+<figure class="image" style="padding: 1rem;">
+  <img src="{path}" alt="{filename}" style="border: solid black 1px;">
+  <figcaption>{caption}</figcaption>
+</figure>"""
+    with open(report_path, "w+") as report:
+        report.write("<h1> Screenshots of test-env: {}</h1>\n".format(test_env_name))
         for test_name, screen_shots in screen_shot_dict.items():
-            screenshot_md.write("\n## Test: {}\n\n".format(test_name))
-            screen_shots_md = map(
-                lambda screen_shot: md_img_template.format(**screen_shot), screen_shots
+            report.write("\n<h2> Test: {}</h2>\n\n".format(test_name))
+            screen_shots_html = map(
+                lambda screen_shot: html_img_template.format(**screen_shot),
+                screen_shots,
             )
-            screenshot_md.writelines(screen_shots_md)
+            report.writelines(screen_shots_html)
 
 
 if __name__ == "__main__":
-    generate_test_screenshot_md()
+    generate_test_screenshot_report()
