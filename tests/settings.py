a707fd951d174aae8f26ca349a079ce6b752c9f0
s-weigand <s.weigand.phy@gmail.com>
2019-05-22 20:27:17 +0200

    now the codce generates two kinds of report, one for local usage and one on the CI for percy to take screenshots of

--- tests/settings.py
+++ tests/settings.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from shutil import rmtree
+
+
 HELPER_SETTINGS = {
     "INSTALLED_APPS": ["djangocms_text_ckeditor", "djangocms_equation"],
     "CMS_LANGUAGES": {1: [{"code": "en", "name": "English"}]},
@@ -9,6 +14,10 @@ HELPER_SETTINGS = {
 
 
 def run():
+    from .generate_screenshot_test_report import get_screenshot_test_base_folder
+    # clean screenshots from last run
+    rmtree(get_screenshot_test_base_folder(), ignore_errors=True)
+    # run test and generate new screenshots
     from djangocms_helper import runner
 
     runner.cms("djangocms_equation")
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -20,16 +20,19 @@ from urllib3.exceptions import NewConnectionError, MaxRetryError
 import percy
 from six.moves.urllib.parse import quote
 
-from .generate_test_screenshot_md import get_screenshot_test_base_folder
+from .generate_screenshot_test_report import (
+    get_screenshot_test_base_folder,
+    generate_test_screenshot_report,
+)
 
 
 class DockerNotFoundException(Exception):
     pass
 
 
-def screen_shot_path(filename, sub_dir=""):
+def screen_shot_path(filename, browser_name, sub_dir=""):
     base_folder = get_screenshot_test_base_folder()
-    dir_path = os.path.join(base_folder, sub_dir)
+    dir_path = os.path.join(base_folder, browser_name, sub_dir)
     if not os.path.isdir(dir_path):
         os.makedirs(dir_path)
     return os.path.join(dir_path, filename)
@@ -66,14 +69,16 @@ def get_browser_instance(browser_port, desire_capabilities, interactive=False):
 
 
 class ScreenCreator:
-    def __init__(self, browser):
+    def __init__(self, browser, browser_name="", use_percy=False):
         self.browser = browser
-        self.run_percy = self.is_on_travis()
+        self.browser_name = browser_name
+        self.run_percy = self.is_on_travis(use_percy)
         self.counter = 0
 
-    def is_on_travis(self):
+    def is_on_travis(self, use_percy):
         if (
             "TRAVIS" in os.environ
+            and use_percy
             and "USE_PERCY" in os.environ
             and os.environ.get("TRAVIS_PULL_REQUEST", "false") != "false"
         ):
@@ -94,16 +99,20 @@ class ScreenCreator:
             )
         else:
             filename = "#{}_{}".format(self.counter, filename)
-            self.browser.save_screenshot(screen_shot_path(filename, sub_dir))
+            self.browser.save_screenshot(
+                screen_shot_path(filename, self.browser_name, sub_dir)
+            )
 
     def init_percy(self):
         # Build a ResourceLoader that knows how to collect assets for this application.
         loader = percy.ResourceLoader(
-            root_dir=settings.STATIC_ROOT,
+            # root_dir=settings.STATIC_ROOT,
+            root_dir=get_screenshot_test_base_folder(),
             base_url=quote(settings.STATIC_URL),
             webdriver=self.browser,
         )
-        self.percy_runner = percy.Runner(loader=loader)
+        percy_config = percy.Config(default_widths=[1200])
+        self.percy_runner = percy.Runner(loader=loader, config=percy_config)
         self.percy_runner.initialize_build()
 
     def stop(self):
@@ -142,15 +151,16 @@ class TestIntegrationChrome(BaseTestCase, StaticLiveServerTestCase):
 
     """
 
-    host = get_own_ip()  # '192.168.178.20'
+    host = get_own_ip()
     browser_port = 4444
     desire_capabilities = DesiredCapabilities.CHROME
+    browser_name = "Chrome"
 
     @classmethod
     def setUpClass(cls):
         super(TestIntegrationChrome, cls).setUpClass()
         cls.browser = get_browser_instance(cls.browser_port, cls.desire_capabilities)
-        cls.screenshot = ScreenCreator(cls.browser)
+        cls.screenshot = ScreenCreator(cls.browser, cls.browser_name)
         cls.wait = ui.WebDriverWait(cls.browser, 10)
         cls.create_test_page()
 
@@ -170,7 +180,15 @@ class TestIntegrationChrome(BaseTestCase, StaticLiveServerTestCase):
         return cls.browser.find_element_by_css_selector(css_selector)
 
     @classmethod
+    def wait_get_element_link_text(cls, link_text):
+        cls.wait.until(lambda driver: driver.find_element_by_link_text(link_text))
+        return cls.browser.find_element_by_link_text("Next")
+
+    @classmethod
     def create_test_page(cls):
+        """
+        Logs in and creates the first page
+        """
         cls.browser.get(cls.live_server_url)
         cls.screenshot.take("initial_page.png", "create_test_page")
         login_form = cls.wait_get_element_css("#login-form")
@@ -184,8 +202,9 @@ class TestIntegrationChrome(BaseTestCase, StaticLiveServerTestCase):
 
         cls.screenshot.take("login-form_filled_out.png", "create_test_page")
         login_form.submit()
+        cls.screenshot.take("user_loged_in.png", "create_test_page")
 
-        next_btn = cls.browser.find_element_by_link_text("Next")
+        next_btn = cls.wait_get_element_link_text("Next")
         next_btn.click()
         cls.browser.switch_to.frame(cls.wait_get_element_css("iframe"))
 
@@ -207,6 +226,38 @@ class TestIntegrationChrome(BaseTestCase, StaticLiveServerTestCase):
         self.assertIn("test_page", body.text)
 
 
-# class TestIntegrationFirefox(TestIntegrationChrome):
-#     browser_remote_address = get_browser_remote_address(4445)
-#     desire_capabilities = DesiredCapabilities.FIREFOX
+class TestIntegrationFirefox(TestIntegrationChrome):
+    browser_port = 4445
+    desire_capabilities = DesiredCapabilities.FIREFOX
+    browser_name = "FireFox"
+
+
+@override_settings(DEBUG=True)
+@override_settings(ALLOWED_HOSTS=["*"])
+@override_settings(STATICFILES_DIRS=(get_screenshot_test_base_folder(),))
+class PercyScreenshotGenerator(StaticLiveServerTestCase):
+    host = get_own_ip()
+    browser_port = 4444
+    desire_capabilities = DesiredCapabilities.CHROME
+
+    @classmethod
+    def setUpClass(cls):
+        super(PercyScreenshotGenerator, cls).setUpClass()
+        cls.browser = get_browser_instance(cls.browser_port, cls.desire_capabilities)
+        cls.screenshot = ScreenCreator(cls.browser, use_percy=True)
+        cls.wait = ui.WebDriverWait(cls.browser, 10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.screenshot.stop()
+        cls.browser.quit()
+        super(PercyScreenshotGenerator, cls).tearDownClass()
+
+    def test_generate_percy_screenshot(self):
+        if self.screenshot.is_on_travis(True):
+            report_filenames = generate_test_screenshot_report(True)
+            for report_filename in report_filenames:
+                self.browser.get(
+                    "{}/static/{}".format(self.live_server_url, report_filename)
+                )
+                self.screenshot.take("{}.png".format(report_filename[:-5]))
