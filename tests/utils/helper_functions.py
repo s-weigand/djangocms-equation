9fbc8201006facb0997c2190c7dcb0c466bcfc7d
s-weigand <s.weigand.phy@gmail.com>
2019-07-18 20:08:35 +0200

    allowed percy upload on not PR's

--- tests/utils/helper_functions.py
+++ tests/utils/helper_functions.py
@@ -69,7 +69,7 @@ class ScreenCreator:
         if (
             "TRAVIS" in os.environ
             and use_percy
-            and os.environ.get("TRAVIS_PULL_REQUEST", "false") != "false"
+            # and os.environ.get("TRAVIS_PULL_REQUEST", "false") != "false"
         ):
             self.init_percy()
             return True
