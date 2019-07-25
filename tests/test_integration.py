5049f36b70c280af632d9e4172c20fcdbfb9c9be
s-weigand <s.weigand.phy@gmail.com>
2019-07-25 18:47:26 +0200

    reduced screenshot width to fit on the report for percy

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -67,7 +67,7 @@ class TestIntegrationChrome(BaseTransactionTestCase, StaticLiveServerTestCase):
             browser_name=cls.browser_name,
         )
         # cls.browser.set_window_size(1366, 768)
-        cls.browser.set_window_size(1200, 1200)
+        cls.browser.set_window_size(1100, 1200)
         cls.screenshot = ScreenCreator(cls.browser, cls.browser_name)
         cls.wait = ui.WebDriverWait(cls.browser, 20)
         cls.browser.delete_all_cookies()
