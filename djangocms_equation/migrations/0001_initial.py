4e738032cbb01a01b1db497ac856fa67b39fb5ad
s-weigand <s.weigand.phy@gmail.com>
2019-10-25 15:02:31 +0200

    partially changed model for django => 2.1

--- djangocms_equation/migrations/0001_initial.py
+++ djangocms_equation/migrations/0001_initial.py
@@ -20,7 +20,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('cmsplugin_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, related_name='djangocms_equation_equationpluginmodel', serialize=False, to='cms.CMSPlugin')),
                 ('tex_code', models.CharField(blank=True, max_length=512, verbose_name='tex_code')),
-                ('is_inline', models.BooleanField(verbose_name='is_inline')),
+                ('is_inline', models.BooleanField(blank=True, verbose_name='is_inline')),
                 ('font_size_value', models.FloatField(default=1, verbose_name='font_size_value')),
                 ('font_size_unit', models.CharField(choices=[('cm', 'cm'), ('mm', 'mm'), ('in', 'in'), ('px', 'px'), ('pt', 'pt'), ('pc', 'pc'), ('em', 'em'), ('ex', 'ex'), ('ch', 'ch'), ('rem', 'rem'), ('vw', 'vw'), ('vh', 'vh'), ('vmin', 'vmin'), ('vmax', 'vmax'), ('%', '%')], max_length=5, verbose_name='font_size_unit')),
             ],
