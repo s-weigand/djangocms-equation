026492e88d3618cf763d6f162cdf123685887480
s-weigand <s.weigand.phy@gmail.com>
2020-09-27 16:18:14 +0200

    Replaced recommonmark with myst_parser for including *.md files in docs

--- docs/refresh_symlinks_windows.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-# Run this in gitbash with administrator rights, if you are on windows
-# and the docs won't build properly.
-export MSYS=winsymlinks:nativestrict
-ln -srf ../README.md readme.md
-ln -srf ../demo.gif demo.gif
-ln -srf ../HISTORY.md history.md
